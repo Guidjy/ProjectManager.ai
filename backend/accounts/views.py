9e19c4766001a7ba43cd9cd6c6da6310e9779786
Guidjy <guippimentel@gmail.com>
2025-06-25 22:18:54 -0300

    added email validation

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,6 +6,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+from email_validator import validate_email, EmailNotValidError
+
 from .models import User
 from .serializers import UserSerializer
 
@@ -29,8 +31,8 @@ class UserViewSet(viewsets.ModelViewSet):
 @permission_classes([AllowAny])
 def register(request):
     """
-    Allows you to create a new user
-    # body:
+    Creates a new user.
+    ### body:
     {
         "username": string,
         "email": string,
@@ -46,6 +48,11 @@ def register(request):
     if not username or not email or not password or not password_confirmation:
         return Response({'error': 'All fields are required.'}, status=400)
     
+    try:
+        email_info = validate_email(email, check_deliverability=True)
+    except EmailNotValidError as e:
+        return Response({'error': f'{e}'}, status=400)
+    
     if password != password_confirmation:
         return Response({'error': 'Password and password confirmation do not match.'}, status=400)
     
@@ -57,13 +64,11 @@ def register(request):
             )
         user.full_clean()
         user.save()
-    except ValidationError as error:
-        return Response({'error': error.message_dict}, status=400)
+    except ValidationError as e:
+        return Response({'error': e.message_dict}, status=400)
     except IntegrityError:
         return Response({'error': 'Username or email already in use.'}, status=400)
     
     serializer = UserSerializer(user)
     
     return Response({'success': 'User successfully created.', 'user': serializer.data}, status=200)
-    
-    
