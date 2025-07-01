1d3801a0e251093404adfaf3acd5fa855f59a1ea
guidjy <guippimentel@gmail.com>
2025-06-30 22:03:16 -0300

    business theme working

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 // tailwind.config.js
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: ["business"],
   },
-}
+};
