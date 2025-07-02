ae095c80d61d3aa75cc17c535d74e3fb5e118408
guidjy <guippimentel@gmail.com>
2025-07-02 14:33:28 -0300

    username state created (LoginPage)

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,9 +2,13 @@
 import LoginRegisterLayout from "../layouts/LoginRegisterLayout";
 // components
 import { Form, FormField } from "../components/Form";
+// hooks
+import { useState } from "react";
 
 
 export default function LoginPage() {
+
+  const [username, setUsername] = useState("");
 
   const fields = [
     { label: "Username", type: "text", placeholder: "username" },
