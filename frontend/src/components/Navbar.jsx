162e2a1aae19f61d95c51f61cf00ba1ce04c9905
Guidjy <guippimentel@gmail.com>
2025-07-11 15:59:29 -0300

    desktop layout for home page created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
   return (
     <>
-      <div className="navbar bg-primary shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm">
         {/* Logo */}
         <div className="flex me-2 md:me-5">
-          <a className="btn btn-primary text-xl">ProjectManager.ai</a>
+          <a className="btn btn-outline btn-primary border-0 text-xl">ProjectManager.ai</a>
         </div>
         {/* Create new project */}
         <div className="flex-1">
           <div className="tooltip tooltip-bottom" data-tip="create new project">
-            <button className="btn btn-circle btn-primary">
+            <button className="btn btn-circle btn-outline btn-primary border-0">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
@@ -21,7 +21,7 @@ export default function Navbar() {
           <div className="dropdown dropdown-end">
           </div>
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-circle btn-primary avatar">
+            <div tabIndex={0} role="button" className="btn btn-circle btn-outline btn-primary border-0 avatar">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,12 +1,21 @@
 // components
 import Navbar from "../components/Navbar"
+// hooks
+import { useBreakpoint } from "../hooks/useBreakpoint"
 
 
 export default function HomeLayout({ children }) {
+
+  const breakPoint = useBreakpoint();
+  
   return (
     <>
       <div className="">
-        <Navbar />
+        {breakPoint === "xs" || breakPoint === "sm" ? (
+          <h1>MOBILE LAYOUT</h1>
+        ) : (
+          <Navbar />
+        )}
         {children}
       </div>
     </>
