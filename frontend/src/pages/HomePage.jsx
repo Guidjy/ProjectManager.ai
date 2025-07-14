8f15203b3ade40f7339eab3b407e54b0276062b4
guidjy <guippimentel@gmail.com>
2025-07-14 17:21:34 -0300

    donwloaded repo on laptop

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -22,6 +22,7 @@ export default function HomePage() {
           </li>
           <li>
             <ProjectButton name="project 2" />
+            0-0
           </li>
         </ul>
       </HomeLayout>
